4f3b0299eeb3f623b7859148247aaa53035e1a48
chliviu <215432+chliviu@users.noreply.github.com>
2020-12-30 16:41:38 +0200

    use collect for the url

--- google_analytics/urls.py
+++ google_analytics/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from google_analytics.views import google_analytics
 
 urlpatterns = [
-    url(r'^google-analytics/$', google_analytics, name='google-analytics'),
+    url(r'^collect/$', google_analytics, name='google-analytics'),
 ]
